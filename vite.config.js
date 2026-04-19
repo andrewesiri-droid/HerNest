a91241d0d99e17bff02be3847530a999e49f4ab8
andrewesiri-droid <andrewesiri@gmail.com>
2026-04-19 14:05:20 -0500

    Move landing page to public folder

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
